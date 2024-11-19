6127fddfd339a157316c1b024d8ff851da4c5222
kubwimanav <kubwimanavincent2020@gmail.com>
2024-11-19 10:55:45 +0200

    profilepage

--- lib/Profile.dart
+++ lib/Profile.dart
@@ -8,12 +8,133 @@ class Profile extends StatelessWidget {
       backgroundColor: Colors.amber,
       appBar: AppBar(),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Container(
-            child: const Text('data'),
+           child: Column(
+            children: [
+              Container(
+
+              child: ClipRRect(
+            borderRadius:const BorderRadius.all(Radius.circular(75)),
+            child: Image.asset('assets/profile.jpg',
+            width: 150,
+            height: 150,
+            fit: BoxFit.cover,
+            ),
+              )
+            
+              ),
+
+              const Text('Lorem Name',style: TextStyle(
+                decoration: TextDecoration.underline,
+                fontSize: 15,
+                fontWeight: FontWeight.w700
+              ),),
+
+
+                   const Text(
+                  'Bonus 27689',
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500),
+                ),
+              
+              
+            ],
+           ),
+          ),
+          Container(
+            height: 500,
+            child: Card(
+              
+              
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+             Container(
+              child:const Row(
+                children: [
+                  Icon(Icons.abc,size: 50,),
+                  Text('Lorem ipsum')
+                ],
+              )
+             ),
+             Container(
+                      child: const Row(
+                    children: [
+                      Icon(
+                        Icons.abc,
+                        size: 50,
+                      ),
+                      Text('Dolar sit')
+                    ],
+                  )),
+                  Container(
+                      child: const Row(
+                    children: [
+                      Icon(
+                        Icons.abc,
+                        size: 50,
+                      ),
+                      Text('Amot Lorem')
+                    ],
+                  )),
+                  Container(
+                      child: const Row(
+                    children: [
+                      Icon(
+                        Icons.abc,
+                        size: 50,
+                      ),
+                      Text('Ipsum dolor')
+                    ],
+                  )),
+                  Container(
+                      child: const Row(
+                    children: [
+                      Icon(
+                        Icons.abc,
+                        size: 50,
+                      ),
+                      Text('Sit Ament')
+                    ],
+                  )),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          child:const Icon(Icons.delivery_dining,
+                          size: 35),
+                        ),
+                     Container(
+                          child: const Icon(Icons.search,
+                          size: 35),
+                        ),
+                        Container(
+                          child: const Icon(Icons.person,
+                          size: 35),
+                        ),
+                      Container(
+                          child: const Icon(Icons.bookmark_border,
+                          size: 35),
+                        ),
+                        Container(
+                          child: const Icon(Icons.settings,
+                          size: 35,),
+                        ),
+                       
+                      ],
+                    ),
+                  )
+            ],
+              ),
+            ),
           )
         ],
       ),
     );
+    
   }
 }
